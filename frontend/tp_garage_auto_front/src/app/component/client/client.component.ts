a020e3bc69d06db1491b2ef6b549bb4b1d116b72
Henri <henri.vanlemmens@etu.unilasalle.fr>
2024-04-09 08:28:46 +0200

    embellisement des component

--- frontend/tp_garage_auto_front/src/app/component/client/client.component.ts
+++ frontend/tp_garage_auto_front/src/app/component/client/client.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { ClientformComponent } from '../clientform/clientform.component';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [],
+  imports: [ClientformComponent, NgForOf],
   templateUrl: './client.component.html',
-  styleUrl: './client.component.css'
+  styleUrl: './client.component.css',
 })
 export class ClientComponent {
-
+  clients = [
+    {
+      id: 1,
+      name: 'John',
+      surname: 'Doe',
+      photo: '1234567890',
+      email: 'joh@sf.fr',
+    },
+    {
+      id: 2,
+      name: 'Jane',
+      surname: 'Doe',
+      email: 'esfsef@zer.fr',
+    },
+  ];
 }
